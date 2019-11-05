b8c6f7fb57064206ad9cde31245d20474bbaaf03
slafi <lafi_saber@yahoo.fr>
2019-11-05 07:10:24 -0500

    Initial release

--- core/export.py
+++ core/export.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from core import location
 
 import logging
+import simplekml
 
 
 # Initialize logger for the module
@@ -9,10 +10,53 @@ logger = logging.getLogger(__name__)
 
 
 def save_as_gpx(filename, data):
-    pass
+    
+    try:
+
+        header = f"""<?xml version="1.0" encoding="UTF-8"?>\n""" \
+                 f"""<gpx version="1.0">\n"""\
+                 f"""<trk><trkseg>\n"""
+        footer = f"""</trkseg></trk></gpx>\n"""
+
+        with open(filename, "a+") as outfile:
+            
+            outfile.write(header)
+
+            for loc in data:
+                trk = ''
+                if loc.mode == 2:
+                    trk = f"""<trkpt lat="{loc.latitude}" lon="{loc.longitude}"><time>{loc.utc_time}</time></trkpt>\n"""
+                elif loc.mode == 3:
+                    trk = f"""<trkpt lat="{loc.latitude}" lon="{loc.longitude}"><ele>{loc.altitude}</ele><time>{loc.utc_time}</time></trkpt>\n"""
+                outfile.write(trk)
+            
+            outfile.write(footer)
+
+    except Exception as e:
+        logger.error(f'Exception: {str(e)}')
 
 
+def save_as_kml(filename, data, name="", description=""):
+    
+    try:
+        kml = simplekml.Kml()
 
-def save_as_kml(filename, data):
-    pass
+        coords=[]
+        for loc in data:
+            coords.append((loc.latitude, loc.longitude))
 
+        kml.newlinestring(name=name, description=description, coords=coords)
+
+        outfile = filename
+        n = len(filename)
+        ext = filename[n-4:n]
+        if(ext.lower() != '.kml'):
+            outfile = f"{filename}.kml"
+
+        kml.save(outfile)
+
+        return 0
+
+    except Exception as e:
+        logger.error(f'Exception: {str(e)}')
+        return -1
